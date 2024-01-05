0b944e204d24d4e3ec88926faf3450c19af5d750
CaioHSCruz <caiohscruz@hotmail.com>
2024-01-05 20:23:37 -0300

    FirstCommit

---
+++ src/RetryAfterAdapter/RetryAfterAdapter/Common/Result.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace RetryAfterAdapter.Common
+{
+    public class Result<TValue, TError>
+    {
+        /// <summary>
+        /// Expected Result of Processing
+        /// </summary>
+        public readonly TValue? Value;
+
+        /// <summary>
+        ///  Processing error
+        /// </summary>
+        public readonly TError? Error;
+
+        private bool _isSuccess;
+
+        private Result(TValue value)
+        {
+            _isSuccess = true;
+            Value = value;
+            Error = default;
+        }
+
+        private Result(TError error)
+        {
+            _isSuccess = false;
+            Value = default;
+            Error = error;
+        }
+
+        // The public static implicit operator Result<TValue, TError>(TValue value) declaration defines an implicit conversion operator.
+        // When a value of type TValue is used in a context where a Result<TValue, TError> is expected (for example, in an assignment),
+        // the compiler can use this operator to automatically convert TValue to a Result<TValue, TError>. Essentially
+        // this simplifies how Result objects are created from TValue values. The same applies to the following statement.
+
+        public static implicit operator Result<TValue, TError>(TValue value) => new Result<TValue, TError>(value);
+
+        public static implicit operator Result<TValue, TError>(TError error) => new Result<TValue, TError>(error);
+
+        /// <summary>
+        /// Processes one or another function depending on the success or failure status of this Result
+        /// </summary>
+        /// <param name="success">
+        /// Function to be executed if the Result in question is successful
+        /// </param>
+        /// <param name="failure">
+        /// Function to be executed if the Result in question is a failure
+        /// </param>
+        /// <returns>
+        /// Returns the executed function result
+        /// </returns>
+        public Result<TValue, TError> Match(Func<TValue, Result<TValue, TError>> success, Func<TError, Result<TValue, TError>> failure)
+        {
+            if (_isSuccess)
+            {
+                return success(Value!);
+            }
+            return failure(Error!);
+        }
+    }
+}
---
+++ src/RetryAfterAdapter/RetryAfterAdapter/Interfaces/ProcessorInterface.cs
@@ -0,0 +1,11 @@
+﻿using RetryAfterAdapter.Common;
+
+namespace RetryAfterAdapter.Interfaces
+{
+    public interface ProcessorInterface<TValue, TError>
+    {
+        public Result<TValue,TError> GetStatus { get; }
+
+        public Result<TValue, TError> IsFailed { get; }
+    }
+}
