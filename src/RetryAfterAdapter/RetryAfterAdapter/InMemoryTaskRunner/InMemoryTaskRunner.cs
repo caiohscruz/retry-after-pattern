155a22c51f534ae99e09dbed52ff3f8758ac2fac
CaioHSCruz <caiohscruz@hotmail.com>
2024-01-05 21:16:55 -0300

    Implementa um TaskRunner em memória

---
+++ src/RetryAfterAdapter/RetryAfterAdapter/InMemoryTaskRunner/InMemoryTaskRunner.cs
@@ -0,0 +1,72 @@
+﻿using RetryAfterAdapter.Common;
+using RetryAfterAdapter.Interfaces;
+using System.Net.Http.Headers;
+
+namespace RetryAfterAdapter.InMemoryProcessor
+{
+    public class InMemoryTaskRunner : ITaskRunner
+    {
+        private static readonly InMemoryTaskRunner instance = new InMemoryTaskRunner();
+        private static Dictionary<string, bool> tasksRunning = new Dictionary<string, bool>();
+
+        private InMemoryTaskRunner() { }
+
+        public static InMemoryTaskRunner Instance => instance;
+
+        public async Task RunTask(Task task, string taskName)
+        {
+            if (tasksRunning.ContainsKey(taskName) && tasksRunning[taskName])
+                return;
+
+            lock (tasksRunning)
+            {
+                if (tasksRunning.ContainsKey(taskName))
+                {
+                    tasksRunning[taskName] = true;
+                }
+                else
+                {
+                    tasksRunning.Add(taskName, true);
+                }
+            }
+
+            try
+            {
+                await task;
+                FinalizeTask(taskName);
+            }
+            catch (Exception)
+            {
+                RemoveTask(taskName);
+            }
+        }
+
+        public void FinalizeTask(string taskName)
+        {
+            if (!Exists(taskName)) return;
+
+            lock (tasksRunning)
+            {
+                tasksRunning[taskName] = false;
+            }
+        }
+
+        public void RemoveTask(string taskName)
+        {
+            lock (tasksRunning)
+            {
+                tasksRunning.Remove(taskName);
+            }
+        }
+
+        public bool Exists(string name)
+        {
+            return tasksRunning.ContainsKey(name);
+        }
+
+        public bool IsRunning(string name)
+        {
+            return tasksRunning[name];
+        }
+    }
+}
---
+++ src/RetryAfterAdapter/RetryAfterAdapter/Interfaces/ITaskRunner.cs
@@ -0,0 +1,14 @@
+﻿using RetryAfterAdapter.Common;
+
+namespace RetryAfterAdapter.Interfaces
+{
+    public interface ITaskRunner
+    {
+        public Task RunTask (Task task, string taskName);
+        public bool Exists(string taskName);
+        public bool IsRunning(string taskName);
+        public void RemoveTask(string taskName);
+        public void FinalizeTask(string taskName);
+
+    }
+}
